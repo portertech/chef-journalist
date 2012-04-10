d697d25675d3889b74b1918bd4141ed09e7a766c
Sean Porter <portertech@gmail.com>
2012-04-10 10:27:08 -0700

    start writing the handler, ready to create a gem

---
+++ chef-journalist.gemspec
@@ -0,0 +1,16 @@
+Gem::Specification.new do |s|
+  s.name        = "chef-journalist"
+  s.version     = "0.0.1"
+  s.authors     = ["Sean Porter"]
+  s.email       = ["portertech@gmail.com"]
+  s.homepage    = "https://github.com/portertech/chef-journalist"
+  s.summary     = "An OpsCode Chef report handler for updated resources"
+  s.description = "An OpsCode Chef report handler for updated resources"
+  s.has_rdoc    = false
+  s.license     = "MIT"
+
+  s.rubyforge_project = "chef-journalist"
+
+  s.files         = `git ls-files`.split("\n")
+  s.require_paths = ["lib"]
+end
---
+++ lib/chef-journalist.rb
@@ -0,0 +1,22 @@
+require 'rubygems'
+require 'chef/handler'
+
+module Chef
+  class Journalist < Chef::Handler
+
+    def initialize(resource_types=[])
+      Chef::Log.info "Generating updated resource report ..."
+      @resource_types = resource_types.map {|t| t.to_sym }
+    end
+
+    def report
+      updated_resources = case
+      when @resource_types.empty?
+        run_status.updated_resources
+      else
+        run_status.updated_resources.select {|r| @resource_types.include? r.resource_name }
+      end
+      updated_resources.each {|r| Chef::Log.info "  #{r.to_s}" }
+    end
+  end
+end
